5193fc57f8930ecff9095a1ef36eb3ca82b7ba47
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2021-07-26 09:32:03 +0200

    Styling changes to match Atlas 3

--- src/ui/styles.ts
+++ src/ui/styles.ts
@@ -7,15 +7,17 @@ export const styles: any = StyleSheet.create({
         flexDirection: "row"
     },
     input: {
-        textAlignVertical: "top",
         flex: 1,
-        padding: 15,
         fontSize: 17.5,
         borderColor: darkMode ? "#333333" : "#cdcdcd",
         borderWidth: 1,
         borderRadius: 5,
         backgroundColor: darkMode ? "#000" : "#FFF",
-        color: darkMode ? "#DEDEDE" : "#212121"
+        color: darkMode ? "#DEDEDE" : "#212121",
+        minWidth: 48,
+        minHeight: 48,
+        paddingHorizontal: 8,
+        paddingVertical: 8
     },
     inputDisabled: {
         backgroundColor: darkMode ? "#141414" : "#f3f3f3"
